59abbb0d928d3d3eb116ba60e7454069691c2472
leonidguadalupe <leonidguadalupe@google.com>
2019-10-08 22:36:49 +0800

    adding hobby data

---
+++ src/data/hobbyObjects.js
@@ -0,0 +1,50 @@
+// Images
+import games from "assets/img/games.jpg";
+import anime from "assets/img/anime.png";
+import food1 from "assets/img/food1.jpg";
+import travel from "assets/img/travel.jpg";
+
+
+const hobbyObjects = [
+  {
+      "xs": 12,
+      "sm": 6,
+      "md": 6,
+      "href": "#travel",
+      "title": "Travel",
+      "description": "I've been travelling a lot lately. I love to explore Japan currently. Here's an image of my fiancé in Akihabara :)",
+      "img": travel
+  },
+  {
+      "xs": 12,
+      "sm": 6,
+      "md": 6,
+      "href": "#games",
+      "title": "Games",
+      "description": "I love playing computer games.\
+      A few titles would be: Zelda(BOTW), Apex Legends\
+      WorldWarZ, AutoChess, PUBG and Mario Odyssey. I love\
+      other shooters as well!",
+      "img": games
+  },
+  {
+    "xs": 12,
+    "sm": 6,
+    "md": 6,
+    "href": "#anime",
+    "title": "Anime",
+    "description": "I love to get some beer and watch my favorite anime during weekend nights. I'm usually updated with the latest anime per season, but I love bungou stray dogs! :)",
+    "img": anime
+  },
+  {
+    "xs": 12,
+    "sm": 6,
+    "md": 6,
+    "href": "#eat",
+    "title": "Eating",
+    "description": "Steak... Nothing beats steak! (or maybe Ramen? yeah Ichiran ramen is life!)",
+    "img": food1
+  }
+]
+
+export default hobbyObjects
